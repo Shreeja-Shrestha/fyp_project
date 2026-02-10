a715665890f07a6c44cb38e8263a8abbae0a0ce1
Shreeja-Shrestha <shresthashreeja136@gmail.com>
2026-02-10 22:09:18 +0545

    message

--- lib/trip_history.dart
+++ lib/trip_history.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
 
+class TripHistoryPage extends StatelessWidget {
+  const TripHistoryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Trip History")),
+      body: const Center(child: Text("Your Trips Will Appear Here")),
+    );
+  }
+}
