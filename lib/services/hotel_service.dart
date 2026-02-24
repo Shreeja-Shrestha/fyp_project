41ae1add129e7b37b862ca5845d2338326406092
Shreeja-Shrestha <shresthashreeja136@gmail.com>
2026-02-24 19:12:02 +0545

    message

--- lib/services/hotel_service.dart
+++ lib/services/hotel_service.dart
@@ -4,10 +4,7 @@ import 'package:http/http.dart' as http;
 class HotelService {
   static const String baseUrl = "http://10.0.2.2:3000/api";
 
-  static Future<Map<String, dynamic>?> fetchNearestHotel(
-    double lat,
-    double lng,
-  ) async {
+  static Future<List<dynamic>> fetchNearbyHotels(double lat, double lng) async {
     try {
       final response = await http.get(
         Uri.parse("$baseUrl/nearest-hotel?lat=$lat&lng=$lng"),
@@ -16,12 +13,11 @@ class HotelService {
       if (response.statusCode == 200) {
         return jsonDecode(response.body);
       } else {
-        print("Server Error: ${response.statusCode}");
-        return null;
+        return [];
       }
     } catch (e) {
-      print("Connection Error: $e");
-      return null;
+      print("Error: $e");
+      return [];
     }
   }
 }
