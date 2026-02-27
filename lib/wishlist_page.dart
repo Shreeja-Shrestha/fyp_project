45b66881f5b4dcc7c5bc81413c26f081e695ec60
Shreeja-Shrestha <shresthashreeja136@gmail.com>
2026-02-27 22:39:54 +0545

    message

--- lib/wishlist_page.dart
+++ lib/wishlist_page.dart
@@ -1,13 +1,1 @@
-import 'package:flutter/material.dart';
 
-class WishlistPage extends StatelessWidget {
-  const WishlistPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Wishlist")),
-      body: const Center(child: Text("Your Wishlist Items")),
-    );
-  }
-}
