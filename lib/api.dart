bf04a2dfb24f3f8790a365c7006505c4c03f2fe4
Shreeja-Shrestha <shresthashreeja136@gmail.com>
2026-03-02 22:17:46 +0545

    message

--- lib/api.dart
+++ lib/api.dart
@@ -1,1 +1,8 @@
-
+class Api {
+  static const String baseUrl =
+      "http://10.0.2.2:3000"; // use your backend IP or localhost
+  static const String addPackage = "$baseUrl/api/packages/add";
+  static const String getPackages = "$baseUrl/api/packages/";
+  static const String updatePackage = "$baseUrl/api/packages/update";
+  static const String deletePackage = "$baseUrl/api/packages/delete";
+}
